c6dcfcb3c9e591fd13a9b29441e113c32565e095
virgoEnigma1981 <dee.mcdonald33@gmail.com>
2019-05-23 22:04:55 -0500

    created the top bar, logo section, and hero section and made them mobile responsive along with responsive drop down navigation

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../fontawesome_5.8.2/fontawesome-free-5.8.2-web/css/all.min.css">
+</head>
+<body>
+<div class="topbar clearfix">
+    <div class="container">
+
+        <div class="topleft">
+            <a href="#">Terms &amp; Conditions</a>
+            <a href="#">FAQS</a>
+            <a href="#">Information</a>
+            <a href="#">Sitemap</a>
+        </div>
+        <div class="topright clearfix">
+            <div class="social_icon">
+                <i class="fab fa-twitter"></i>
+            </div>
+            <div class="social_icon">
+                <i class="fas fa-rss"></i>
+            </div>
+            <div class="social_icon">
+                <i class="fab fa-pinterest"></i>
+            </div>
+            <div class="social_icon">
+                <i class="fab fa-facebook-f"></i>
+            </div>
+        </div>
+    </div>
+    
+</div>
+
+<div class="logobar clearfix">
+    <div class="container">
+        <div class="logobox email">
+            <i class="fas fa-envelope"></i> <i>hotelsupport@gmail.com</i>
+        </div>
+        <div class="logobox logo">
+            <img src="./img/37-hotel-logos.gif" alt="">
+        </div>
+        <div class="logobox phone">
+            <i class="fas fa-phone"></i> <i>800-885-8585</i>
+        </div>
+    </div>
+</div>
+
+<div class="navbar clearfix">
+    <div class="container">
+        <div class="links">
+            <a href="#home">home</a>
+            <a href="#home">ACCOMMODATION</a>
+            <a href="#home">ABOUT US</a>
+            <a href="#home">GALLERY</a>
+            <a href="#home">TESTIMONIALS</a>
+            <a href="#home">NEWS</a>
+            <a href="#home">CONTACT US</a>
+        </div>
+        <div class="navbtn clearfix">
+            <div class="bars"></div>
+            <div class="bars"></div>
+            <div class="bars"></div>
+        </div>
+    </div>
+</div>
+
+<div class="hero">
+    <div class="container">
+
+    </div>
+</div>
+
+
+
+<script src="js/script.js"></script>
+<script src="../fontawesome_5.8.2/fontawesome-free-5.8.2-web/js/all.min.js"></script>
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,7 @@
+let navBtn = document.querySelector('.navbtn');
+let showNavigation = document.querySelector('.links');
+
+navBtn.addEventListener('click', function(){
+    showNavigation.classList.toggle('show');
+})
+
