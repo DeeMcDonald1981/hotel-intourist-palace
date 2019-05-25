7d4c3050ba21d4f753fe4f57bf76ad2cb7dc37ca
virgoEnigma1981 <dee.mcdonald33@gmail.com>
2019-05-24 20:12:05 -0500

    added hotel ammenities testimonials and footer section

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="../fontawesome_5.8.2/fontawesome-free-5.8.2-web/css/all.min.css">
 </head>
 <body>
-<div class="topbar clearfix">
+<div id="home" class="topbar clearfix">
     <div class="container">
 
         <div class="topleft">
@@ -87,8 +87,27 @@
             </div>   
 
             <h4>grand executive suited</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime incidunt non eligendi consectetur laborum voluptate autem voluptatem, est a repellendus ex repudiandae voluptas, perferendis nobis alias. Vitae veniam blanditiis repellat ducimus, iste dolore voluptate accusamus perspiciatis ea iure, suscipit ipsam!</p>
-        </div>
+            <p class="info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime incidunt non eligendi consectetur laborum voluptate autem voluptatem, est a repellendus ex repudiandae voluptas, perferendis nobis alias. Vitae veniam blanditiis repellat ducimus, iste dolore voluptate accusamus perspiciatis ea iure, suscipit ipsam!</p>
+            <div class="ammenities clearfix">
+                    <div class="check_container">
+                        <i class="far fa-check-square"></i>
+                        <p>Quality Linens</p>
+                    </div>
+
+
+                    <div class="check_container">
+                        <i class="far fa-check-square"></i>
+                        <p>Individual Thermostat Control</p>
+                    </div>
+
+
+                    <div class="check_container">
+                        <i class="far fa-check-square"></i>
+                        <p>Fireplace and jetted tubs</p>
+                    </div>
+
+            </div>
+        </div><!-- row -->
 
         <div class="row">
             <div class="img_container">
@@ -98,24 +117,171 @@
                 <img src="./img/bedroom-690129_960_720.jpg" alt="">
             </div>   
             <h4>Deluxe Room</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime incidunt non eligendi consectetur laborum voluptate autem voluptatem, est a repellendus ex repudiandae voluptas, perferendis nobis alias. Vitae veniam blanditiis repellat ducimus, iste dolore voluptate accusamus perspiciatis ea iure, suscipit ipsam!</p>
+            <p class="info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime incidunt non eligendi consectetur laborum voluptate autem voluptatem, est a repellendus ex repudiandae voluptas, perferendis nobis alias. Vitae veniam blanditiis repellat ducimus, iste dolore voluptate accusamus perspiciatis ea iure, suscipit ipsam!</p>
+            
+            <div class="ammenities clearfix">
+                    <div class="check_container">
+                        <i class="far fa-check-square"></i>
+                        <p>Plenty of soft and fluffy towels</p>
+                    </div>
 
-        </div>
+
+                    <div class="check_container">
+                        <i class="far fa-check-square"></i>
+                        <p>High speed Wi-fi access in every room</p>
+                    </div>
+
+
+                    <div class="check_container">
+                        <i class="far fa-check-square"></i>
+                        <p>Kitchenettes (limited Availability)</p>
+                    </div>
+
+            </div>
+
+        </div><!-- row -->
 
         <div class="row">
-            <div class="img_container">
-            <div class="booknow">
+            <div class="img_container clearfix">
+            <div class="booknow clearfix">
             <p>book now! </p><i class="far fa-arrow-alt-circle-right"></i>
                 </div>
                 <img src="./img/hotel-room-1447201_960_720.jpg" alt="">
             </div>   
 
             <h4>Traditional Room</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime incidunt non eligendi consectetur laborum voluptate autem voluptatem, est a repellendus ex repudiandae voluptas, perferendis nobis alias. Vitae veniam blanditiis repellat ducimus, iste dolore voluptate accusamus perspiciatis ea iure, suscipit ipsam!</p>
+            <p class="info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime incidunt non eligendi consectetur laborum voluptate autem voluptatem, est a repellendus ex repudiandae voluptas, perferendis nobis alias. Vitae veniam blanditiis repellat ducimus, iste dolore voluptate accusamus perspiciatis ea iure, suscipit ipsam!</p>
+            <div class="ammenities clearfix">
+                    <div class="check_container">
+                        <i class="far fa-check-square"></i>
+                        <p>The beds are extremly comfortable</p>
+                    </div>
 
+
+                    <div class="check_container">
+                        <i class="far fa-check-square"></i>
+                        <p>Telephone</p>
+                    </div>
+
+
+                    <div class="check_container">
+                        <i class="far fa-check-square"></i>
+                        <p>Hair Dryer</p>
+                    </div>
+
+            </div>
+
+        </div><!-- row -->
+    </div><!-- container -->
+</div><!-- rooms -->
+
+<div class="testimonials clearfix">
+    <div class="container">
+        <div class="hotel_features clearfix">
+            <h3>hotel features</h3>
+            <div class="feature_box">
+
+                <i class="fas fa-chevron-circle-up"></i>
+                <p>Fast Check-in / Check-Out</p>
+            </div>
+            <div class="feature_box">
+
+                <i class="fas fa-chevron-circle-up"></i>
+                <p>Free Self Parking for Cars</p>
+            </div>
+            <div class="feature_box">
+
+                <i class="fas fa-chevron-circle-up"></i>
+                <p>Site Seeing Tours</p>
+            </div>
         </div>
-    </div>
+        <div class="clients">
+            
+            <h3>Client Testimonials</h3>
+            <div class="text_box">
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate ullam esse vitae, voluptates iure tenetur impedit itaque beatae corrupti excepturi atque, nobis accusamus quaerat natus inventore fugit asperiores aliquam? Expedita asperiores aspernatur eos possimus, vitae nesciunt totam vel itaque quo fuga accusantium a temporibus id mollitia quisquam dicta optio dolore ab incidunt commodi exercitationem autem non. Culpa, soluta perspiciatis. Aperiam fugit perferendis minima, aspernatur sequi repellendus delectus facere eveniet! Inventore?</p>
+            </div>
+        </div>
+   </div>
 </div>
+
+<footer class="footer clearfix">
+    <div class="container clearfix">
+        <div class="box">
+            <h3>get in touch</h3>
+            <form >
+                <input type="text" placeholder="Name">
+                <input type="email" placeholder="Email">
+                <textarea name="" id="" cols="30" rows="5" placeholder="Message"></textarea>
+                <input type="submit">
+            </form>
+        </div><!-- box -->
+
+        <div class="box clearfix">
+            <h3>gallery</h3>
+            <div class="gallery  clearfix">
+                <div class="gallery_row clearfix ">
+                    
+                    <div class="gallery_box">
+                        <img src="./img/hotel-room-1447201_960_720.jpg" alt="">
+                    </div>
+                    <div class="gallery_box">
+                        <img src="./img/hotel-room-1447201_960_720.jpg" alt="">
+                    </div>
+                    <div class="gallery_box">
+                        <img src="./img/hotel-room-1447201_960_720.jpg" alt="">
+                    </div>
+                </div><!-- gallery_row -->
+                <div class="gallery_row clearfix">
+                    
+                    <div class="gallery_box">
+                        <img src="./img/hotel-room-1447201_960_720.jpg" alt="">
+                    </div>
+                    <div class="gallery_box">
+                        <img src="./img/hotel-room-1447201_960_720.jpg" alt="">
+                    </div>
+                    <div class="gallery_box">
+                        <img src="./img/hotel-room-1447201_960_720.jpg" alt="">
+                    </div>
+                </div><!-- gallery_row -->
+            </div><!-- gallery -->
+        </div><!-- box -->
+
+        <div class="box hours">
+            <h3>hours</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur ex neque nisi libero optio labore alias tempora temporibus sit, architecto commodi? Itaque quis praesentium quia ab cum, porro excepturi dolorem.</p>
+            <p><span> Mon-Fri: 10AM to 5PM</span></p>
+            <p><span> Sat-Sun: 10AM to 2PM</span></p>
+        </div><!-- box -->
+
+        <div class="box contact">
+            <h3>contact us</h3>
+            <div class="font_box">
+                <i class="fas fa-home box_color"></i>
+                <span>9207 Greenrose St. New York, NY 10024</span>
+            </div>
+            <div class="font_box">
+                <i class="fas fa-phone box_color"></i>
+                <span>hotelsupport@gmail.com</span>
+            </div>
+            <div class="font_box">
+                <i class="fas fa-envelope box_color"></i>
+                <span>800-885-8585</span>
+            </div>
+        </div><!-- box -->
+
+        <div class="clearfix"></div>
+        <div class="copyright clearfix">
+            <div class="container">
+                <p>&copy; <?php echo date('Y')?> Intourist Hotel, All Rights Reserved </p>
+                <a href="#home"><i class="fas fa-chevron-circle-up"></i></a>
+
+
+            </div>
+        </div>
+
+    </div><!-- container -->
+</footer>
 
 
 
