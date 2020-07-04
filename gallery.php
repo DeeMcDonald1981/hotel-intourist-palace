fa181ff398d0dfbdade3b3cd336e98ba9c5798d8
virgoEnigma1981 <dee.mcdonald33@gmail.com>
2020-07-04 18:30:24 -0500

    Added images carousel to the gallery page and updated CSS, HTML and Javascript files

--- gallery.php
+++ gallery.php
@@ -4,6 +4,23 @@ include('inc/header.php');
 <div class="container">
   <section id="about">
     <h1 class="intro">gallery</h1>
+    <div class="carousel-container">
+
+      <span id="prevBtn"><i class="fas fa-arrow-left"></i></span>
+      <span id="nextBtn"><i class="fas fa-arrow-right"></i></span>
+
+      <div class="carousel-slide">
+        <img src="./img/img_gallery/img5.jpg" alt="" id="lastClone">
+        <img src="./img/img_gallery/img1.jpg" alt="">
+        <img src="./img/img_gallery/img2.jpg" alt="">
+        <img src="./img/img_gallery/img3.jpg" alt="">
+        <img src="./img/img_gallery/img4.jpg" alt="">
+        <img src="./img/img_gallery/img5.jpg" alt="">
+        <img src="./img/img_gallery/img1.jpg" alt="" id="firstClone">
+      </div><!-- carousel-slide -->
+
+    </div><!-- carousel-container -->
+
   </section>
 </div>
 <?php
--- js/script.js
+++ js/script.js
@@ -21,3 +21,65 @@ const stickyNav = () => {
 }
 stickyNav();
 
+
+function galleryPage() {
+    const carouselSlide = document.querySelector('.carousel-slide');
+    const carouselImages = document.querySelectorAll('.carousel-slide img');
+
+    const prevBtn = document.querySelector('#prevBtn');
+    const nextBtn = document.querySelector('#nextBtn');
+
+
+    let counter = 1;
+    const size = carouselImages[0].clientWidth;
+    console.log(size);
+
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+    nextBtn.addEventListener('mouseover', () => {
+        if (counter >= carouselImages.length - 1) return;
+        carouselSlide.style.transition = 'transform .8s ease-in-out';
+        counter++;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+    });
+    nextBtn.addEventListener('click', () => {
+        if (counter >= carouselImages.length - 1) return;
+        carouselSlide.style.transition = 'transform .8s ease-in-out';
+        counter++;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+    });
+
+    prevBtn.addEventListener('mouseover', () => {
+        if (counter <= 0) return;
+        carouselSlide.style.transition = 'transform .8s ease-in-out';
+        counter--;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+    });
+    
+    prevBtn.addEventListener('click', () => {
+        if (counter <= 0) return;
+        carouselSlide.style.transition = 'transform .8s ease-in-out';
+        counter--;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+    });
+
+    carouselSlide.addEventListener('transitionend', () => {
+        if (carouselImages[counter].id === 'lastClone') {
+            carouselSlide.style.transition = 'none';
+            counter = carouselImages.length - 2;
+            carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+        }
+
+        if (carouselImages[counter].id === 'firstClone') {
+            carouselSlide.style.transition = 'none';
+            counter = carouselImages.length - counter;
+            carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+        }
+    });
+
+}
+galleryPage();
