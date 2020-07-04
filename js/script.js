33ccd65fc9273235889418589397a9378677e0be
virgoEnigma1981 <dee.mcdonald33@gmail.com>
2020-07-03 19:59:31 -0500

    created includes folder, created template pages, updates the css/sass styles, and add more javascript functionality

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
-let navBtn = document.querySelector('.navbtn');
-let showNavigation = document.querySelector('.links');
+const mobileNav = () => {
+    let navBtn = document.querySelector('.navbtn');
+    let showNavigation = document.querySelector('.links');
 
-navBtn.addEventListener('click', function(){
-    showNavigation.classList.toggle('show');
-})
+    navBtn.addEventListener('click', function () {
+        showNavigation.classList.toggle('show');
+    })
+}
+mobileNav();
+
+const stickyNav = () => {
+    let navbar = document.querySelector('.navbar');
+    window.addEventListener('scroll', () => {
+        let totalNav = navbar.scrollHeight + navbar.offsetTop;
+        if (window.scrollY > totalNav + 50) {
+            navbar.classList.add('stickyNav');
+        } else {
+            navbar.classList.remove('stickyNav');
+        }
+    });
+}
+stickyNav();
 
